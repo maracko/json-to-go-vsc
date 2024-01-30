7daf105e8e1ff5ce8d0cc30af914cf9ea8c247a8
Mario Petričko <petricko.mario@gmail.com>
2024-01-31 00:27:00 +0100

    big commit, most of code changed
    New feature:
    
    - Output selection. Now includes clipboard, current file and the previous default temporary file
        * The old `inputSource` setting has been renamed to input and will be migrated automatically
    
    Improved error handling, now including more and more detailed messages
    Refactors most of the code, making it more robust and better documentated.
    These changes fix potential bugs setTimeout could have caused, and should fix setting update context (WIP)
    Removed package-lock.json in favor of yarn.lock

--- extension.js
+++ extension.js
@@ -3,144 +3,305 @@ const tmp = require('tmp');
 const fs = require('fs');
 const jsonToGo = require('./json-to-go.js');
 
+const enums = {
+  input: {
+    clipboard: 'clipboard',
+    selection: 'selection',
+    askMe: 'ask me every time',
+  },
+  output: {
+    clipboard: 'clipboard',
+    temporaryFile: 'temporary file',
+    cursorPosition: 'cursor position',
+    askMe: 'ask me every time',
+  },
+  Remember: {
+    Yes: 'Yes',
+    No: 'No',
+    DontAsk: "No and don't ask again",
+  },
+  Button: {
+    ShowDetails: 'Show details',
+    DontShowAgain: "Don't show again",
+  },
+};
 
-
-/**
- * Updates context for the right mouse context menu with values from settings.
- * Done like this because VS code didn't update context after a setting update for some reason
- **/
-function setSupportedLanguagesContext() {
-	let settings = vscode.workspace.getConfiguration('json-to-go')
-
-	/** @type {Array} */
-	let supportedLanguages = settings.get('contextMenu.supportedLanguages')
-	vscode.commands.executeCommand('setContext', 'json-to-go:contextMenuLanguages', supportedLanguages)
-
-	let allLanguages = supportedLanguages.includes('*')
-	vscode.commands.executeCommand('setContext', 'json-to-go:contextMenuAlways', allLanguages)
-}
+const keys = {
+  context: {
+    askRememberInput: 'askRememberInput',
+    askRememberOutput: 'askRememberOutput',
+    notifyClipboardOutput: 'notifyClipboardOutput',
+    contextMenuLanguages: 'contextMenuLanguages',
+    contextMenuAlways: 'contextMenuAlways',
+  },
+  settings: {
+    contextMenu: {
+      supportedLanguages: 'contextMenu.supportedLanguages',
+    },
+    input: 'input',
+    /* ! Deprecated: replaced by input */
+    inputSource: 'inputSource',
+    output: 'output',
+    inlineTypeDefinitions: 'inlineTypeDefinitions',
+  },
+  commands: {
+    convert: 'convert',
+  },
+  jsonToGo: 'json-to-go',
+};
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	setSupportedLanguagesContext()
+  let settings = getConfig();
+  let supportedLanguages = settings.get(keys.settings.contextMenu.supportedLanguages);
+  vscode.commands.executeCommand('setContext', longKey(keys.context.contextMenuLanguages, ':'), supportedLanguages);
+  vscode.commands.executeCommand('setContext', longKey(keys.context.contextMenuAlways, ':'), supportedLanguages.includes('*'));
 
+  // migrate inputSource to input
+  let inputSource = settings.get(keys.settings.inputSource);
+  if (inputSource) {
+    settings.update(keys.settings.input, inputSource, vscode.ConfigurationTarget.Global);
+    settings.update(keys.settings.inputSource, undefined, vscode.ConfigurationTarget.Global);
+  }
+  setDefault(context, keys.context.askRememberInput, true);
+  setDefault(context, keys.context.askRememberOutput, true);
+  setDefault(context, keys.context.notifyClipboardOutput, true);
 
-	//Initial setter for 'remember choice' context
-	if (typeof context.globalState.get('json-to-go.askRememberChoice') === 'undefined') {
-		context.globalState.update('json-to-go.askRememberChoice', true)
-	}
+  // register listener for context menu setting changes
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration(keys.settings.contextMenu.supportedLanguages)) {
+      (async () => {
+        let supportedLanguages = settings.get(keys.settings.contextMenu.supportedLanguages);
+        console.log(`longKeys: ${longKey(keys.context.contextMenuLanguages, ':')} | ${longKey(keys.context.contextMenuAlways, ':')}`);
+        await vscode.commands.executeCommand('setContext', longKey(keys.context.contextMenuLanguages, ':'), supportedLanguages);
+        await vscode.commands.executeCommand('setContext', longKey(keys.context.contextMenuAlways, ':'), supportedLanguages.includes('*'));
+      })();
+    }
+  });
 
-	let convertFunc = vscode.commands.registerCommand('json-to-go.convert', () => { convert(context) })
-	context.subscriptions.push(convertFunc)
+  let convertFunc = vscode.commands.registerCommand(longKey(keys.commands.convert), () => {
+    convert(context);
+  });
+  context.subscriptions.push(convertFunc);
 }
 
-
-function deactivate() {
-
-}
-
+/*
+ * TODO: Potential cleanup needed */
+function deactivate() {}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function convert(context) {
+  try {
+    let settings = getConfig();
+    let textToConvert = '';
+    let input = settings.get(keys.settings.input);
+    if (input === enums.input.askMe) {
+      input = await vscode.window.showQuickPick([capitalize(enums.input.clipboard), capitalize(enums.input.selection)], {
+        placeHolder: 'Select input source (can be changed inside extension settings)',
+      });
+      if (!input) {
+        vscode.window.showInformationMessage('Nothing chosen, aborting');
+        return;
+      }
+      input = input.toLowerCase();
+    }
+    let askRememberInput = context.globalState.get(longKey(keys.context.askRememberInput));
+    if (askRememberInput) {
+      let rememberInput = await vscode.window.showQuickPick(...capitalizedValues(enums.Remember), {
+        placeHolder: 'Remember input?',
+      });
 
-	try {
-		setSupportedLanguagesContext()
+      switch (rememberInput) {
+        case enums.Remember.Yes:
+          settings.update(keys.settings.input, input, vscode.ConfigurationTarget.Global);
+          break;
+        case enums.Remember.DontAsk:
+          context.globalState.update(longKey(keys.context.askRememberInput), false);
+          break;
+        case enums.Remember.No:
+        case undefined:
+          break;
+        default:
+          weirdThrow(`Remember input "${rememberInput}"`);
+      }
+    }
 
-		let inputSource = vscode.workspace.getConfiguration('json-to-go').get('inputSource')
-		let textToConvert = ''
+    switch (input) {
+      case enums.input.selection:
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          vscode.window.showErrorMessage('No editor active for input');
+          return;
+        }
+        let selection = editor.selection;
+        textToConvert = editor.document.getText(selection);
+        break;
+      case enums.input.clipboard:
+        textToConvert = await vscode.env.clipboard.readText();
+        break;
+      default:
+        weirdThrow(`Input "${input}"`);
+    }
 
-		if (inputSource === 'ask me every time') {
-			inputSource = await vscode.window.showQuickPick(['Clipboard', 'Selection'], { placeHolder: 'Select input source (can be changed inside extension settings)' })
-			inputSource = inputSource.toLowerCase()
+    if (!textToConvert) {
+      vscode.window.showErrorMessage('No text to convert', ...[enums.Button.ShowDetails]).then((btn) => {
+        if (btn === enums.Button.ShowDetails) {
+          vscode.window.showInformationMessage(input == enums.input.clipboard ? 'Nothing in clipboard' : 'No text is selected in the editor');
+        }
+      });
+      return;
+    }
 
-			let askRememberChoice = context.globalState.get('json-to-go.askRememberChoice')
-			if (askRememberChoice) {
-				let rememberChoice = await vscode.window.showQuickPick(['Yes', 'No', 'No and don\'t ask again'], { placeHolder: 'Remember choice?' });
+    let inline = settings.get(keys.settings.inlineTypeDefinitions);
+    let struct = jsonToGo(textToConvert, null, !inline);
+    if (struct.error) {
+      vscode.window.showErrorMessage('Invalid JSON', ...[enums.Button.ShowDetails]).then((btn) => {
+        if (btn === enums.Button.ShowDetails) {
+          vscode.window.showInformationMessage(struct.error);
+        }
+      });
+      return;
+    }
 
-				switch (rememberChoice) {
-					case 'Yes':
-						vscode.workspace.getConfiguration('json-to-go').update('inputSource', inputSource.toLowerCase(), vscode.ConfigurationTarget.Global);
-						break;
-					case 'No and don\'t ask again':
-						context.globalState.update('json-to-go.askRememberChoice', false);
-						break;
-				}
-			}
+    let output = settings.get(keys.settings.output);
+    if (output === enums.output.askMe) {
+      output = await vscode.window.showQuickPick(
+        ...[capitalize(enums.output.clipboard), capitalize(enums.output.cursorPosition), capitalize(enums.output.temporaryFile)],
+        {
+          placeHolder: 'Select output destination',
+        }
+      );
+      if (!output) {
+        vscode.window.showInformationMessage('Nothing chosen, aborting');
+        return;
+      }
+      output = output.toLowerCase();
 
-		}
+      let askRememberOutput = context.globalState.get(longKey(keys.context.askRememberOutput));
+      if (askRememberOutput) {
+        let rememberOutput = await vscode.window.showQuickPick(...capitalizedValues(enums.Remember), {
+          placeHolder: 'Remember output?',
+        });
 
-		switch (inputSource) {
-			case 'selection':
-				let editor = vscode.window.activeTextEditor;
-				if (!editor) {
-					vscode.window.showErrorMessage('No editor active');
-					return;
-				}
+        switch (rememberOutput) {
+          case enums.Remember.Yes:
+            settings.update(keys.settings.output, output, vscode.ConfigurationTarget.Global);
+            break;
+          case enums.Remember.DontAsk:
+            context.globalState.update(longKey(keys.context.askRememberOutput), false);
+            break;
+          case enums.Remember.No:
+            break;
+          default:
+            weirdThrow(`Remember output "${rememberOutput}"`);
+        }
+      }
+    }
 
-				let selection = editor.selection;
-				textToConvert = editor.document.getText(selection);
-				break;
-			case 'clipboard':
-				textToConvert = await vscode.env.clipboard.readText()
-				break;
-		}
+    switch (output) {
+      case enums.output.clipboard:
+        await vscode.env.clipboard.writeText(struct.go);
+        if (settings.get(keys.context.notifyClipboardOutput))
+          vscode.window.showInformationMessage('Go struct copied to clipboard', ...[enums.Button.DontShowAgain]).then((selection) => {
+            if (selection === enums.Button.DontShowAgain) {
+              settings.update(keys.context.notifyClipboardOutput, false, vscode.ConfigurationTarget.Global);
+            }
+          });
+        break;
+      case enums.output.cursorPosition:
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          vscode.window.showErrorMessage('No editor active for output');
+          return;
+        }
+        editor.edit((editBuilder) => {
+          editBuilder.insert(editor.selection.active, struct.go);
+        });
+        break;
+      case enums.output.temporaryFile:
+        tmp.file({ prefix: 'GoStruct-', postfix: '.go', keep: false }, async function (err, path, fd) {
+          if (err) {
+            vscode.window.showErrorMessage('Temp file error', ...[enums.Button.ShowDetails]).then((btn) => {
+              if (btn === enums.Button.ShowDetails) {
+                vscode.window.showInformationMessage(`Error: "${err.name}: ${err.message}"
+                Path: "${path}
+                File descriptor: "${fd}"`);
+              }
+            });
+            return;
+          }
 
-		if (!textToConvert) {
-			vscode.window.showErrorMessage('No text to convert');
-			return;
-		}
+          fs.writeFileSync(path, struct.go);
 
-		let inline = vscode.workspace.getConfiguration('json-to-go').get('inlineTypeDefinitions');
-		let struct = jsonToGo(textToConvert, null, !inline);
-		if (struct.error) {
-			vscode.window.showErrorMessage('Invalid JSON')
-			return
-		}
+          let openPath = vscode.Uri.file(path);
+          let doc = await vscode.workspace.openTextDocument(openPath);
+          await vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, false);
 
-		tmp.file({ prefix: 'GoStruct-', postfix: '.go', keep: false }, function (err, path) {
-			if (err) {
-				vscode.window.showErrorMessage(err)
-			}
-			fs.writeFileSync(path, struct.go);
+          await vscode.commands.executeCommand('cursorMove', {
+            to: 'right',
+            by: 'character',
+            value: 5,
+          });
 
-			let openPath = vscode.Uri.file(path)
-			vscode.workspace.openTextDocument(openPath).then(doc => {
-				vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, false)
-					.then(() => {
-						setTimeout(() => {
-							vscode.commands.executeCommand('cursorMove', {
-								to: 'right',
-								by: 'character',
-								value: 5
-							})
-						},
-							50)
-
-						setTimeout(() => {
-							vscode.commands.executeCommand('cursorMove', {
-								to: 'right',
-								by: 'character',
-								value: 13,
-								select: true
-							})
-						},
-							55)
-					})
-			})
-		})
-
-	} catch (err) {
-		console.log("Error in json-to-go:", err)
-	}
+          await vscode.commands.executeCommand('cursorMove', {
+            to: 'right',
+            by: 'character',
+            value: 13,
+            select: true,
+          });
+        });
+        break;
+      default:
+        weirdThrow(`Output "${output}"`);
+    }
+  } catch (err) {
+    vscode.window.showErrorMessage(`Error in ${keys.jsonToGo}-vsc`, ...[enums.Button.ShowDetails]).then((btn) => {
+      if (btn === enums.Button.ShowDetails) {
+        vscode.window.showInformationMessage(`Error Name: "${err.name}" | Error Message: "${err.message}" | Stack Trace: "${err.stack}"`);
+      }
+    });
+  }
 }
 
+/* concat key with main key */
+function longKey(key, sep = '.') {
+  return keys.jsonToGo + sep + key;
+}
+
+/* get configuration */
+function getConfig() {
+  return vscode.workspace.getConfiguration(keys.jsonToGo);
+}
+
+/* set default value for key if not already set */
+function setDefault(context, key, value) {
+  if (typeof context.globalState.get(longKey(key)) === 'undefined') {
+    context.globalState.update(longKey(key), value);
+  }
+}
+
+/* capitalize first letter of string */
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* capitalize all values of an object and return as array */
+function capitalizedValues(obj) {
+  return Object.values(obj).map((val) => capitalize(val));
+}
+
+/* man this is weird */
+function weirdThrow(weird) {
+  if (weird) throw new Error(`${weird} is weird`);
+  else throw new Error('Something is weird');
+}
 
 module.exports = {
-	activate,
-	deactivate,
-	convert
-}
+  activate,
+  deactivate,
+  convert,
+};
